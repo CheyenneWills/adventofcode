cc8bcc2d8451adaa55d206f32aa0e3eb9d0e919c
Cheyenne Wills <cwills@witznd.net>
2016-12-20 07:32:00 -0700

    updates

--- 2016/md5sum.c
+++ 2016/md5sum.c
@@ -1,16 +1,20 @@
 #include <openssl/md5.h>
 #include "icall.h"
 
-int MD5sum(int argc,descriptor argv[])  
+/* gcc -I/usr/lib/icon/lib -lcrypto -shared -o md5sum.so -fPIC md5sum.c */
+
+/* Given a string, returns a binary string of the md5sum */
+
+int MD5sum(int argc,descriptor argv[])
 {
     MD5_CTX c;
-    char *s;      
+    char *s;
     int slen;
     unsigned char out[MD5_DIGEST_LENGTH];
 
     MD5_Init(&c);
     ArgString(1);
-    
+
     s = StringAddr(argv[1]);
     slen = StringLen(argv[1]);
     MD5_Update(&c,s,slen);
