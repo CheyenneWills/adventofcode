a43e4166b00beeb912f71be1fc3e6fd3fe6a8c22
Cheyenne Wills <cheyenne.wills@ricoh-usa.com>
2017-12-26 11:52:15 -0700

    Misc cleanup of other years

--- 2015/day4/p4.py
+++ 2015/day4/p4.py
@@ -7,7 +7,7 @@ suffix = ""
 i = 0
 while True:
     h = hashlib.md5(v + suffix).hexdigest()
-    if h.startswith("000000"):
+    if h.startswith("00000"):
         break
     i += 1
     suffix = str(i)
